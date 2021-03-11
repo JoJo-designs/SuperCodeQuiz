31b1bdd2ada3be59d79bdc8136e586ebf7e156dd
jojo <jodiladouceur.design@gmail.com>
2021-03-11 10:28:31 -0500

    edits to files an new file added

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Building a quiz about code.
 The goal of this project is to build a quiz with codeing related questions. The quiz will contain a few questions that are mulitpile choose. The quiz will be timed and the return a score based on the number of correct answers given. The use will be able to save thier names and score so after playing another game the name an previous score will still appear.
 
 No starter files were provided.
+
+What I started with so far.
+3 html files one for the landing page and the second that the quiz will live one
+the quiz file has places for the timer the question and 4 possiable answers. The
+last page is where the user will see their score and input a name. 
+
+So far I have the frist page linked to the quiz. On the quiz page a timer works and
+when time runs out it send the user to a the last page. I have a function that will 
+also take users to the end page when they answerd all the questions
+
+My next step is to make a single question work and when it is answerd the user is
+sent to the end page.
--- asset/script.js
+++ asset/script.js
@@ -1,1 +1,57 @@
-console.log("here is the page. the java script is connected")
+//making sure javascript file is attacted to quiz.html
+console.log("Here is the page. The java script is connected");
+
+
+var timeBox = document.querySelector("#timeBox");
+var questionContain = document.querySelector("#questionContainer");
+
+var qestion = document.querySelector("#question");
+var buttonClick = document.querySelector ("button");
+var answerA = document.querySelector("#answerOne");
+var answerB = document.querySelector("#answerTwo");
+var answerC = document.querySelector("#answerThree");
+var answerD = document.querySelector("#answerFour");
+
+var score = 0;
+var useName;
+var timeInterval;
+
+
+//funtion that runs the timer
+function countDown() {
+    var maxTime = 10;
+
+  var timeInterval = setInterval(function () {
+    timeBox.textContent = maxTime;
+    maxTime --;
+
+    if ( maxTime === -1){
+      clearInterval(timeInterval);
+      console.log("Game Over")
+      window.location.href = "end.html"
+    }
+  }, 1000);
+}
+
+// function ends the game when a question is answered or at least it should.
+function endGame() {
+    window.location.href = "end.html"
+    console.log("Game Over")
+    clearInterval(timeInterval);
+    
+}
+
+//buttonClick.addEventListener("onclick", function() {});
+
+//Question one
+function askQuestionOne() {
+        qestion.textContent = "What is 'Something'?";
+        answerA.textContent = "Everything";
+        answerB.textContent = "Nothing";
+        answerC.textContent = "Anything";
+        answerD.textContent = "None of the above";
+   buttonClick.addEventListener("click", function() { });
+}
+
+countDown();
+askQuestionOne();
