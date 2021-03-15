c2743af28fd6b3d415c1f7df416b2fde4fb24caa
jojo <jodiladouceur.design@gmail.com>
2021-03-15 13:08:39 -0400

    edits to scriptTwo.js

--- asset/scriptTwo.js
+++ asset/scriptTwo.js
@@ -4,17 +4,18 @@ var perams = {
 }
 
 var highScore = document.querySelector("#highScoreBox");
-var nameValue = document.querySelector("#highScoreName");
+var nameValue = document.querySelector("#nameValue");
 var scoreValue = document.querySelector("#placeForScore");
 var userName = document.querySelector("#nameBox").value;
 var submitButton = document.querySelector("#submit");
 
 var lastScore; //save The last score that has played
-var playName = document.getElementById("nameBox").value  
 
 var score = localStorage.getItem("score");
 var lastScore = localStorage.getItem("lastScore");
 var lastPlayer = localStorage.getItem("Name");
+
+
 
 //listens to the button to be clicked
 submitButton.addEventListener("click", function(event) {
@@ -25,9 +26,12 @@ submitButton.addEventListener("click", function(event) {
       });
       perams.playName.push(document.getElementById("nameBox").value);
       console.log(perams.playName)
-      localStorage.setItem("Name", perams.playName)
+      localStorage.setItem("Name", JSON.stringify(perams.playName))
     saveLastScore();
     });
+
+
+
 
 // Saves the users pervious score as a sepreate score. 
 function saveLastScore() {
@@ -35,11 +39,11 @@ function saveLastScore() {
 // The last score will change when the user clicks thet submit button
 } 
 
-// fill in the content for the areas that need to be filled in
+
+
+// fill in the content for the areas that need to be filled in the namevalue is not working
 highScore.textContent = lastScore; 
 nameValue.textContent = perams.playName;
 scoreValue.textContent = score;
 
-// need to collect the name from the user and set it to local storage
-// under the name lastPlayer
 
