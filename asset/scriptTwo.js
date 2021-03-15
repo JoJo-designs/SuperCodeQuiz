9c9386a4af60a08d0a2ca669d0833118dae4b3f4
jojo <jodiladouceur.design@gmail.com>
2021-03-15 11:00:23 -0400

    edits to scriptTwo.js

--- asset/scriptTwo.js
+++ asset/scriptTwo.js
@@ -1,24 +1,30 @@
+//setting veruables 
 var scoreValue = document.querySelector("#placeForScore");
-var userName = document.querySelector("#nameBox");
+var userName = document.querySelector("#nameBox").value;
 var highScore = document.querySelector("#highScoreBox");
 var submitButton = document.querySelector("#submit");
 var lastScore; 
-var lastPlayer;
+var lastPlayer; // save the name the last user.
 
 var score = localStorage.getItem("score");
+var lastScore = localStorage.getItem("lastScore");
 
 
+//listens to the button to be clicked
 submitButton.addEventListener("click", function(event) {
     event.preventDefault();
     document.getElementById("nameBox").innerHTML = localStorage.getItem("namebox");
     saveLastScore();
 });
 
+
+// Saves the users perivose score as a sepreate score. 
 function saveLastScore() {
     localStorage.setItem("lastScore", score)
 }
 
-document.getElementById("nameBox").innerHTML = localStorage.getItem("namebox");
-
+// fill in the content for the areas that need to be filled in
+highScore.textContent = lastScore; // display the last user name beside the last score
 scoreValue.textContent = score;
-highScore.textContent = lastScore;
+
+// need to collect the name from the user and set it to local storage
