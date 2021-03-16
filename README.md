c45522b0ca7a824bb4030ed177e0f104b743dfeb
jojo <jodiladouceur.design@gmail.com>
2021-03-16 14:05:05 -0400

    lots of changes to many files. A few new files added

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ Building a quiz about code.
 
 The goal of this project is to build a quiz with codeing related questions. The quiz will contain a few questions that are mulitpile choose. The quiz will be timed and the return a score based on the number of correct answers given. The use will be able to save thier names and score so after playing another game the name an previous score will still appear.
 
+find the repository here:
+Live Here:
+
 No starter files were provided.
+
 
 What I started with so far.
 3 html files one for the landing page and the second that the quiz will live one
@@ -20,4 +24,13 @@ button was clicked and be able to know if it is correct or incorrect
 
 I have made four questions work and when the last question is anwesed the user is sent to an end page and the score is saved. the use can input their name and click a button. the score will be saved as last score but at the moment the name doesn't save. when I get the name to save I think I can start to make it look pretty.
 
-I think I finally have everything working right. it was hard to get here and I needed to reach out for help a couple times. 
+
+Afterwards
+
+Like most project this one had it's own issues. However when I compare this to the project before it this one was a little easier. I would guess it was becauase I am now more familliar with the technologies that are being used. The issues came from the javascript and I ran into two major issues. The fisrt was getting the event listener to work for the buttons. Around the same time I needed the if statment to select the right value. I went to class office hours and the issue was resloved. I was able to continue until I needed to save the inputed players name in localstorage. Again I reached out for help and after some confusion and work I was able to make it work. After I had it functioning I realized that even when the submit button was click the name wouldn't update until the page was refreshed. I wanted the high score values to update when the submit button was click so I tired a few thing to make it work. In the end I create another function that would update the high scores. I tired to work step by step for this project and I didn't use much CSS until I had everything working. I only make 4 questions but I think I may be able to add additional questions pretty easily. The pages are reponsive to the screen size but it could be a little cleaner. 
+
+Screen shots 
+![The landing page](/image/img1.png?raw=true "Landing Page")
+![The frist question](/image/img2.png?raw=true "Question")
+![The score page](/image/img3.png?raw=true "final score")
+![ran out of time page ](/image/img4.png?raw=true "ran out of time")
--- asset/script.js
+++ asset/script.js
@@ -7,6 +7,7 @@ var questionContain = document.querySelector("#questionContainer");
 
 var qestion = document.querySelector("#question");
 var buttonClick = document.querySelector("button");
+var additional = document.querySelector("#stuffHere");
 var answerA = document.querySelector("#answerOne");
 var answerB = document.querySelector("#answerTwo");
 var answerC = document.querySelector("#answerThree");
@@ -16,14 +17,16 @@ var score = 0;
 var useName;
 var timeInterval;
 var element;
-var questions = ["What is 'Something'?", "What is 'Nothing'?", "This is a question?"];
+var questions = ["Q.1: What does this function do?", "Q.2: Which statement is true?", "Q.3: What is the name of the first page of every Website on the internet?", "Q:4 What information is needed to make an '<img>' tag work"];
 var functionArray = [];
 var nextQuestion;
 functionArray.push(askQuestionOne);
 
+
+
 //funtion that runs the timer
 function countDown() {
-    var maxTime = 100;
+    var maxTime = 60;
 
   var timeInterval = setInterval(function () {
     timeBox.textContent = maxTime;
@@ -32,7 +35,7 @@ function countDown() {
     if ( maxTime === -1){
       clearInterval(timeInterval);
       console.log("Game Over")
-      window.location.href = "end.html"
+      window.location.href = "incomplete.html"
     }
   }, 1000);
 }
@@ -67,45 +70,49 @@ function nextQuestion() {
 //Questions this area will ask the questions.
   function askQuestionOne() {
   qestion.textContent = questions[0];
-  answerA.textContent = "Everything";
-  answerB.textContent = "Nothing";
-  answerC.textContent = "Anything";
-  answerD.textContent = "None of the above";
+  answerA.textContent = "Randomly picks from an array called arrayOfCharacters.";
+  answerB.textContent = "creates and returns a variable called item.";
+  answerC.textContent = "Two of these are right";
+  answerD.textContent = "Does it work?";
   functionArray[0] = (askQuestionTwo);
   console.log(functionArray)
 };
 
  function askQuestionTwo() {
   qestion.textContent = questions[1];
-  answerA.textContent = "Everythin";
-  answerB.textContent = "Nothin";
-  answerC.textContent = "Anythin";
-  answerD.textContent = "None of the abov";
+  additional.textContent = "";
+  answerA.textContent = "HTML stands for Hyper-text Makeup language.";
+  answerB.textContent = "CSS stands for Cascading Style sheet.";
+  answerC.textContent = "Java Script is the hardest programming language to learn.";
+  answerD.textContent = "None of the above.";
   functionArray[0] = (askQuestionThree);
   console.log(functionArray)
 };
 
  function askQuestionThree() {
   qestion.textContent = questions[2];
-  answerA.textContent = "Everythi";
-  answerB.textContent = "Nothi";
-  answerC.textContent = "Anythi";
-  answerD.textContent = "None of the abo";
+  answerA.textContent = "website.html";
+  answerB.textContent = "inbex.html";
+  answerC.textContent = "page_1.html";
+  answerD.textContent = "index.html";
   functionArray[0] = (askQuestionFour);
 };
 
 function askQuestionFour() {
-  qestion.textContent = questions[0];
-  answerA.textContent = "Everyth";
-  answerB.textContent = "Noth";
-  answerC.textContent = "Anyth";
-  answerD.textContent = "None of the ab";
+  qestion.textContent = questions[3];
+  answerA.textContent = "alt=''";
+  answerB.textContent = "href=''";
+  answerC.textContent = "src=''";
+  answerD.textContent = "1 and 3";
   functionArray[0] = (endGame);
 };
 
 // This will check the answers of the questions
 function checkQuestion() {
-        if (element.textContent === "Anything" || element.textContent === "Everythin" ) {
+        if (element.textContent === "Two of these are right" || 
+        element.textContent === "CSS stands for Cascading Style sheet." || 
+        element.textContent === "index.html" ||
+        element.textContent === "1 and 3") {
             score = score + 10
             console.log ("correct")
             console.log (score)
@@ -115,7 +122,6 @@ function checkQuestion() {
             console.log (score)
             nextQuestion();
 }};
-
 
 countDown();
 askQuestionOne()
